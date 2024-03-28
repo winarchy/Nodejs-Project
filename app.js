77383d6b6147f037f96d29c03863a1a23f15652e
winarchy <83733357+winarchy@users.noreply.github.com>
2024-03-28 16:07:40 +0700

    Sending Responses

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req);
-//   process.exit();
+  //   process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body>Hello from my Node.js Server!</body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
